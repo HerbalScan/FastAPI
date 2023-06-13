ec841ed6d8663d488e8e974beb76a2e10d664ff0
Elham Herlambang <125782064+elhamhp@users.noreply.github.com>
2023-06-13 09:45:59 +0700

    Update main.py
    Menambahkan fucntion: Membuat logic untuk mengambil informasi tumbuhan dari Firestore berdasarkan predict class

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ from PIL import Image
 import numpy as np
 from json import dumps
 from uvicorn import run
+import firebase_admin
+from firebase_admin import credentials, firestore
+from collections import OrderedDict
 
 app = FastAPI()
 
@@ -17,20 +20,24 @@ methods = ["*"]
 headers = ["*"]
 
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins = origins,
-    allow_credentials = True,
-    allow_methods = methods,
-    allow_headers = headers    
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=methods,
+    allow_headers=headers
 )
 
 model_dir = "model_v2.h5"
 model = load_model(model_dir)
 
 classes = np.array(['alang_alang', 'andong', 'jambu_biji', 'jarong', 'kumis_kucing',
-'kunyit', 'lengkuas', 'lidah_buaya', 'mengkudu', 'pacar_air',
-'saga', 'sambiloto', 'seledri', 'serai', 'sirih'
-])
+                    'kunyit', 'lengkuas', 'lidah_buaya', 'mengkudu', 'pacar_air',
+                    'saga', 'sambiloto', 'seledri', 'serai', 'sirih'])
+
+# Initialize Firebase Admin SDK with service account
+cred = credentials.Certificate("./serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 @app.get("/")
 async def root():
@@ -60,11 +67,31 @@ async def create_upload_file(file: bytes = File(...)):
             "Score": f"{model_score:0.2f} %"
         }
 
-    return {
-        "Prediction": predicted_class,
-        "Score": f"{model_score:0.2f} %"
-    }
-    
-if _name_ == "_main_":
-    port = int(os.environ.get('PORT', 8080))
-    run(app, host="0.0.0.0", port=port)
+    # Get plant information from Firestore based on predicted class
+    doc_ref = db.collection("Tanaman").document(predicted_class)
+    doc = doc_ref.get()
+    if doc.exists:
+        plant_data = doc.to_dict()
+        ordered_plant_data = OrderedDict([
+            ("namaTumbuhan", plant_data.get("informasi").get("namaTumbuhan")),
+            ("deskripsi", plant_data.get("informasi").get("deskripsi")),
+            ("caraPengobatan", plant_data.get("informasi").get("caraPengobatan")),
+            ("caraPengobatan2", plant_data.get("informasi").get("caraPengobatan2")),
+            ("caraPengobatan3", plant_data.get("informasi").get("caraPengobatan3")),
+            ("caraPengobatan4", plant_data.get("informasi").get("caraPengobatan4"))
+        ])
+        # Menghapus kunci yang memiliki nilai None
+        ordered_plant_data = {k: v for k, v in ordered_plant_data.items() if v is not None}
+        return {
+            "Prediction": predicted_class,
+            "Score": f"{model_score:0.2f} %",
+            "PlantInfo": {
+                "informasi": ordered_plant_data
+            }
+        }
+    else:
+        return {
+            "Prediction": predicted_class,
+            "Score": f"{model_score:0.2f} %",
+            "PlantInfo": "Plant information not found"
+        }
